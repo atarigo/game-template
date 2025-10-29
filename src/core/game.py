f38330bfa1d585f733ff478ec1dfe8f72123f66f
wenke.lai <wenke.studio@gmail.com>
2025-10-29 15:28:32 +0800

    fix: fix the wrong imports

--- src/core/game.py
+++ src/core/game.py
@@ -2,24 +2,21 @@ import pygame
 import structlog
 
 from src.setup.config import Settings
-from src.state import GameState, GameStateManager
+from src.state import GameState, GameStateManager, SceneState
 
 from .events import EventManager
-from .scenes import SceneManager, SceneState
+from .scenes import SceneManager
 
 logger = structlog.get_logger(__name__)
 
 
 class Game:
-    def __init__(self, settings: Settings):
+    def __init__(self, settings: Settings, events: EventManager, scenes: SceneManager):
         self.settings = settings
-        self.events = EventManager()
+        self.events = events
+        self.scenes = scenes
 
-        # managers
-        self.state = GameStateManager(events=self.events)
-
-        self.scene = SceneManager(events=self.events)
-        register_scenes(scene_manager=self.scene)
+        self.state = GameStateManager(events=events)
         self.events.emit(SceneState.SwitchTo, {"scene_name": "landing"})
 
         # initialize pygame
@@ -50,22 +47,15 @@ class Game:
                 self.events.emit(GameState.Quitting)
 
             if event.type == pygame.KEYDOWN:
-                self.scene.handle_event(keydown=event)
+                self.scenes.handle_event(keydown=event)
 
     def update(self, dt: float):
         self.events.process_events()
-        self.scene.update(dt)
+        self.scenes.update(dt)
 
     def draw(self):
         self.screen.fill((0, 0, 0))
 
-        self.scene.draw(self.screen)
+        self.scenes.draw(self.screen)
 
         pygame.display.flip()
-
-
-def register_scenes(scene_manager: SceneManager):
-    from src.scenes import CityScene, LandingScene
-
-    scene_manager.register("landing", LandingScene)
-    scene_manager.register("city", CityScene)
