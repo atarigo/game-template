6423d58e374dfa12b91d2c0f0fc61b8479ad0d73
wenke.lai <wenke.studio@gmail.com>
2025-10-31 20:20:57 +0800

    feat: add jf openhuninn font

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import logging
+from pathlib import Path
 
 import pygame
+import structlog
 
 from plugins.client import Game
+from plugins.core.fonts import install_default_font_override
 from plugins.core.logs import configure
 from plugins.event import EventManager
 from plugins.scene import SceneEvent, SceneEventData, SceneManager
@@ -10,8 +13,18 @@ from plugins.scene import SceneEvent, SceneEventData, SceneManager
 from .scenes import CityScene, InstanceScene, LandingScene, PlaygroundScene, StoreScene
 from .setup.config import Settings
 
+logger = structlog.get_logger(__name__)
 
-def prepare(settings):
+
+def prepare(settings: Settings):
+    # Set custom default font to jf-openhuninn-2.1.ttf
+    font_path = Path("./src/assets/fonts/jf-openhuninn-2.1.ttf")
+    if font_path.exists():
+        install_default_font_override(font_path)
+        logger.info("Font installed", font=font_path)
+    else:
+        logger.error("Font file not found", font=font_path)
+
     pygame.init()
     pygame.display.set_mode(settings.window.size)
     pygame.display.set_caption(settings.window.title)
@@ -21,6 +34,8 @@ def launch():
     settings = Settings()
     configure(logging.DEBUG)
     prepare(settings)
+
+    prepare()
 
     events = EventManager()
 
